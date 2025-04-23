ef287620387a39e184eb8584924d16d51daac127
Eric Black <eric.black@vmlyr.com>
2025-04-23 15:52:15 -0600

    install commerce core & set up config sync

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -256,7 +256,7 @@ $databases = [];
  * directory in the public files path. The setting below allows you to set
  * its location.
  */
-# $settings['config_sync_directory'] = '/directory/outside/webroot';
+$settings['config_sync_directory'] = '../config/sync';
 
 /**
  * Settings:
@@ -883,4 +883,3 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
-$settings['config_sync_directory'] = 'sites/default/files/config_bqT2Is1VB7VCvX01Zfzpf2gSeJZc7L5ybo9e-KXuHw1nv1CCf2JWO65lL_c8LKCxWaIQzHv7LQ/sync';
